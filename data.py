89a85f9192a3727b6b8e68d3eb23981985c37910
Leona-cloud <chisomokafor038@gmail.com>
2024-06-14 15:51:33 +0100

    chore: first attempt

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles(): 
+    articles = [
+        {
+            'id': 1,
+            'title': 'article1',
+            'body': 'orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'author': 'Leona',
+            'created_at': '24-09-1999'
+        },
+         {
+            'id': 2,
+            'title': 'article2',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'author': 'Leona Ada',
+            'created_at': '26-09-1999'
+        },
+         {
+            'id': 3,
+            'title': 'article3',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'author': 'Chisom',
+            'created_at': '29-09-1999'
+        }
+    ]
+    return articles
